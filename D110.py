3e2b158bcede98c826a0b4773aebc81679f8f8c2
Benson7472174 <98572896+Benson7472174@users.noreply.github.com>
2025-02-17 23:42:46 +0800

    Update D110.py

--- D110.py
+++ D110.py
@@ -1,1 +1,10 @@
-
+def min_king_moves(start, end):
+    x1 = ord(start[0]) - ord('a')
+    y1 = int(start[1]) - 1
+    x2 = ord(end[0]) - ord('a')
+    y2 = int(end[1]) - 1
+    return max(abs(x1 - x2), abs(y1 - y2))
+start = input().strip()
+end = input().strip()
+steps = min_king_moves(start, end)
+print(steps)
